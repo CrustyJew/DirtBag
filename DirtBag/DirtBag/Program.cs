99c89ce8b381e021d935002074fbf73451340c86
CrustyJew <mattiprice520@gmail.com>
2015-10-09 14:09:23 -0500

    Load settings from Wiki Add more app config options Add event to settings change

--- DirtBag/DirtBag/Program.cs
+++ DirtBag/DirtBag/Program.cs
@@ -5,29 +5,51 @@ using System.Text;
 using System.Threading.Tasks;
 using RedditSharp;
 using System.Configuration;
+using System.Threading;
 
 namespace DirtBag {
 	class Program {
 		public static WebAgent Agent { get; set; }
 		public static Reddit Client { get; set; }
+		public static RedditAuth Auth { get; set; }
+		public static BotSettings Settings { get; set; }
+
+		private static ManualResetEvent waitHandle = new ManualResetEvent( false );
+		public const double VersionNumber = 1.0;
 		static void Main( string[] args ) {
 			//Instantiate and throw away a Reddit instance so the static constructor won't interfere with the WebAgent later.
 			new Reddit();
 
 			Initialize();
 
-			System.Threading.Thread.Sleep( System.Threading.Timeout.Infinite ); //Go the fuck to sleep
+			waitHandle.WaitOne(); //Go the fuck to sleep
 			
 		}
 
 		public static void Initialize() {
+			string uAgent = ConfigurationManager.AppSettings["UserAgentString"] ;
+			string sub = ConfigurationManager.AppSettings["Subreddit"];
+			if ( string.IsNullOrEmpty( uAgent ) ) throw new Exception( "Provide setting 'UserAgentString' in AppConfig to avoid Reddit throttling!" );
+			if ( string.IsNullOrEmpty( sub ) ) throw new Exception( "Provide setting 'Subreddit' in AppConfig" );
+
 			Agent = new WebAgent();
 			WebAgent.EnableRateLimit = true;
 			WebAgent.RateLimit = WebAgent.RateLimitMode.Burst;
 			WebAgent.RootDomain = "oauth.reddit.com";
-            if ( string.IsNullOrEmpty( ConfigurationManager.AppSettings["UserAgentString"] ) ) throw new Exception( "Provide setting 'UserAgentString' in AppConfig to avoid Reddit throttling!" );
-			WebAgent.UserAgent = ConfigurationManager.AppSettings["UserAgentString"];
-			
+           			WebAgent.UserAgent = uAgent;
+			Auth = new RedditAuth();
+			Auth.Login();
+
+			Client = new Reddit( Auth.AccessToken );
+
+			Settings = new BotSettings();
+			Settings.OnSettingsModified += Settings_OnSettingsModified;
+			Settings.Subreddit = "GoAwayNoOneLikesYou";
+            Settings.Initialize( Client );
+		}
+
+		private static void Settings_OnSettingsModified( object sender, EventArgs e ) {
+			System.Diagnostics.Debug.WriteLine( "Received settings modified event" );
 		}
 	}
 }
--- DirtBag/DirtBag/RedditAuth.cs
+++ DirtBag/DirtBag/RedditAuth.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Configuration;
 using RedditSharp;
 namespace DirtBag {
@@ -14,8 +14,8 @@ namespace DirtBag {
 		private string pass;
 		private string clientID;
 		private string clientSecret;
-
-		private TimerState timerState;
+		private string redirectURI;
+        private TimerState timerState;
 
 		private const int FIFTYFIVE_MINUTES = 3300000;
 		
@@ -24,15 +24,17 @@ namespace DirtBag {
 			pass = ConfigurationManager.AppSettings["BotPassword"];
 			clientID = ConfigurationManager.AppSettings["ClientID"];
 			clientSecret = ConfigurationManager.AppSettings["ClientSecret"];
+			redirectURI = ConfigurationManager.AppSettings["RedirectURI"];
 			if ( string.IsNullOrEmpty( uname ) ) throw new Exception( "Missing 'BotUsername' in config" );
 			if ( string.IsNullOrEmpty( pass ) ) throw new Exception( "Missing 'BotPassword' in config" );
 			if ( string.IsNullOrEmpty( clientID ) ) throw new Exception( "Missing 'ClientID' in config" );
 			if ( string.IsNullOrEmpty( clientSecret ) ) throw new Exception( "Missing 'ClientSecret' in config" );
+			if ( string.IsNullOrEmpty( redirectURI ) ) throw new Exception( "Missing 'RedirectURI' in config" );
 			timerState = new TimerState();
 		}
 		public void GetNewToken() {
 			try {
-				AuthProvider ap = new AuthProvider( clientID, clientSecret, "/" );
+				AuthProvider ap = new AuthProvider( clientID, clientSecret, redirectURI );
 				AccessToken = ap.GetOAuthToken( uname, pass );
 			}
 			catch { //TODO error handling
@@ -43,8 +45,8 @@ namespace DirtBag {
 
 		public void Login() {
 			timerState.TimerRunning = true;
-			timerState.TimerRef = new System.Threading.Timer( new System.Threading.TimerCallback( RefreshTokenTimer ), timerState, 0, FIFTYFIVE_MINUTES );
-          
+			timerState.TimerRef = new Timer( new TimerCallback( RefreshTokenTimer ), timerState, FIFTYFIVE_MINUTES, FIFTYFIVE_MINUTES );
+			GetNewToken();
 		}
 
 		private void RefreshTokenTimer(object s ) {
@@ -60,7 +62,7 @@ namespace DirtBag {
 
 		private class TimerState {
 			public bool TimerRunning;
-			public System.Threading.Timer TimerRef;
+			public Timer TimerRef;
 		}
 
 		
