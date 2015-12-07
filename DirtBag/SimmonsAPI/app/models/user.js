4c960e630447d37fbeed607e5bdd9fc21086b144
patrick-motard <motard19@gmail.com>
2015-12-06 20:10:32 -0600

    added user schema

---
+++ DirtBag/SimmonsAPI/app/models/user.js
@@ -0,0 +1,14 @@
+﻿// app/models/user.js
+
+var mongoose    = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    userName: String,
+    accountCreatedDate: Date,
+    isAdmin: Boolean,
+    lastLogin: Date,
+    isActive: Boolean
+});
+
+module.exports = mongoose.model('User', UserSchema);
--- DirtBag/SimmonsAPI/server.js
+++ DirtBag/SimmonsAPI/server.js
@@ -1,6 +1,44 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿// server.js
+
+// BASE SETUP
+// ======================================================================================================
+
+// call the packages we need
+var express = require('express');                               // call express
+var app = express();                                        // define our app using express
+var bodyParser = require('body-parser');
+var User = require('./app/models/user');
+
+// configure app to use bodyParser()
+// this will let us get data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8087;                                    // set our port
+
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017');
+var db = mongoose.connect;
+
+
+
+// ROUTES FOR API
+// =======================================================================================================
+var router = express.Router();                                          // get an instance of the express router
+
+// test route to make sure everything is working (accessed at GET htttp://localhost:8080/api
+router.get('/', function (req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for SimmonsAPI go here
+
+// REGISTER ROUTES
+// =======================================================================================================
+
+app.use('/api', router);
+
+// START THE SERVER
+// =======================================================================================================
+app.listen(port);
+console.log('magic happens on port ' + port);
