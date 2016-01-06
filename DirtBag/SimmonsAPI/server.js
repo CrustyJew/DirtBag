8593f87d48f41daa112535eb85765060f4b3eb07
Patrick Motard <motard19@gmail.com>
2016-01-06 13:31:55 -0600

    - added route api/authenticate, takes in name and password, returns token or     error message - added middleware to check for token before returning routes ~/api/users/* or     ~/api/

--- DirtBag/SimmonsAPI/server.js
+++ DirtBag/SimmonsAPI/server.js
@@ -66,6 +66,66 @@ router.use(function(req, res, next) {
     next(); //make sure we go to the next routes and dont stop here
 });
 
+// route to authenticate user (POST http://localhost:8087/api/authenticate)
+router.post('/authenticate', function(req, res){
+
+  // find user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user){
+    if(err) throw err;
+    if(!user){
+      res.json({success: false, message: 'Authentication failed. User not found.'});
+    } else if (user){
+      // check if password matches
+      if(user.password != req.body.password){
+        res.json({success: false, message: "Authentication failed. Wrong password"});
+      } else {
+        // if user is found and password is right, create tokens
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as json
+        res.json({
+          success: true,
+          message: "Enjoy your token!",
+          token: token
+        });
+      }
+    }
+  })
+})
+
+
+// route middleware to verify a token
+router.use(function(req, res, next){
+  //check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  //decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded){
+      if(err){
+        return res.json({ success: false, message: 'Failed to authenticate token.'});
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    // if there is no token, return an errors
+    return res.status(403).send({
+      sucess: false,
+      message: 'No token provided.'
+    })
+  }
+});
+
+
 // test route to make sure everything is working (accessed at GET htttp://localhost:8080/api
 router.get('/', function (req, res) {
   res.send('Hello! The API is at http://localhost:' + port + '/api');
