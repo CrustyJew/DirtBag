ea3d18ae2b5740b4a32a7d98bc670bae718b91d1
Meepster <mattiprice520@gmail.com>
2016-03-20 23:12:25 -0500

    More work on automod wrangler

--- DirtBag/DirtBag/BotFunctions/AutoModWrangler.cs
+++ DirtBag/DirtBag/BotFunctions/AutoModWrangler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,11 @@ namespace DirtBag.BotFunctions {
         private const string AUTOMOD_WIKI_PAGE = "config/automoderator";
         private const string WIKI_SECTION_START_IDENTIFIER = "#***DIRTBAG BOT SECTION***";
         private const string WIKI_SECTION_END_IDENTIFIER = "#***END DIRTBAG BOT SECTION***";
-        private const string MATCH_PARAMETER = "author+media_author_url: ";
+
+        private string wikiContent = "";
+        private int startBotSection;
+        private int botSectionLength;
+
         public AutoModWrangler() {
         }
 
@@ -19,36 +24,45 @@ namespace DirtBag.BotFunctions {
             SubReddit = subReddit;
         }
 
-        public List<string> GetBannedAuthorsAndChannels() {
-            string config = GetAutomodConfig();
-            int ulistStart = config.IndexOf( MATCH_PARAMETER ) + MATCH_PARAMETER.Length;
-            int ulistLen = config.IndexOf( "\r\n", ulistStart ) - ulistStart;
-            string ulistRaw = config.Substring( ulistStart, ulistLen );
+        public bool AddToBanList( IEnumerable<DirtBag.Models.BannedEntity>) {
+            
 
-            //remove square brackets
-            ulistRaw = ulistRaw.Substring( 1 );
-            ulistRaw = ulistRaw.Substring( 0, ulistRaw.Length - 1 );
-
-            return ulistRaw.Split( ',' ).ToList();
+            return true;
         }
 
-        public string GetAutomodConfig() {
+        private bool SaveBanList(IEnumerable<string> entities ) {
+            string newWikiContent = wikiContent.Substring( 0, startBotSection - 1 );
+        }
 
-            RedditSharp.WikiPage automodWiki = SubReddit.Wiki.GetPage( AUTOMOD_WIKI_PAGE );
+        private string GetAutomodConfig() {
+
+            RedditSharp.WikiPage automodWiki;
+            try {
+                automodWiki = SubReddit.Wiki.GetPage( AUTOMOD_WIKI_PAGE );
+                wikiContent = automodWiki.MarkdownContent;
+            }
+            catch ( WebException ex ) {
+                if ( ( ex.Response as HttpWebResponse ).StatusCode == HttpStatusCode.NotFound ) {
+                    wikiContent = WIKI_SECTION_START_IDENTIFIER + GetDefaultBotConfigSection() + WIKI_SECTION_END_IDENTIFIER;
+                }
+                else {
+                    throw;
+                }
+            }
 
             string botConfigSection = "";
-            if ( !automodWiki.MarkdownContent.Contains( WIKI_SECTION_START_IDENTIFIER ) ) return "";
-            if ( !automodWiki.MarkdownContent.Contains( WIKI_SECTION_END_IDENTIFIER ) ) return "";
-            int startPos = automodWiki.MarkdownContent.IndexOf( WIKI_SECTION_START_IDENTIFIER ) + WIKI_SECTION_START_IDENTIFIER.Length;
-            int configLen = automodWiki.MarkdownContent.IndexOf( WIKI_SECTION_END_IDENTIFIER, startPos ) - startPos;
-            botConfigSection = automodWiki.MarkdownContent.Substring( startPos, configLen );
+            if ( !wikiContent.Contains( WIKI_SECTION_START_IDENTIFIER ) ) return "";
+            if ( !wikiContent.Contains( WIKI_SECTION_END_IDENTIFIER ) ) return "";
+            startBotSection = wikiContent.IndexOf( WIKI_SECTION_START_IDENTIFIER ) + WIKI_SECTION_START_IDENTIFIER.Length;
+            botSectionLength = wikiContent.IndexOf( WIKI_SECTION_END_IDENTIFIER, startBotSection ) - startBotSection;
+            botConfigSection = wikiContent.Substring( startBotSection, botSectionLength );
             return botConfigSection;
         }
 
         private string GetDefaultBotConfigSection() {
             string config = @"
 ---
-author+media_author_url: []
+author+media_author_url: [{0}]
 action: remove
 action_reason: ""Dirtbag Banned Author/Channel : {{match}}""
 priority: 9001
---
+++ DirtBag/DirtBag/Models/BannedEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirtBag.Models {
+    public class BannedEntity {
+        public string EntityString { get; set; }
+        public string BannedBy { get; set; }
+        public string BanReason { get; set; }
+        public DateTime? BanDate { get; set; }
+        public string Link { get; set; }
+
+    }
+}
