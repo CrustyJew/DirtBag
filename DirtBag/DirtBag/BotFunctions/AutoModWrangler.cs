58109a06eae943c9a47232d55b960ec71a7b7f9f
Meepster <mattiprice520@gmail.com>
2016-04-22 11:58:28 -0500

    Re-organize automod wrangler

--- DirtBag/DirtBag/BotFunctions/AutoModWrangler.cs
+++ DirtBag/DirtBag/BotFunctions/AutoModWrangler.cs
@@ -20,21 +20,37 @@ namespace DirtBag.BotFunctions {
         public AutoModWrangler() {
         }
 
-        public AutoModWrangler(RedditSharp.Things.Subreddit subReddit ) {
+        public AutoModWrangler( RedditSharp.Things.Subreddit subReddit ) {
             SubReddit = subReddit;
         }
 
-        public bool AddToBanList( IEnumerable<DirtBag.Models.BannedEntity>) {
-            
+        public async Task<bool> AddToBanList( IEnumerable<DirtBag.Models.BannedEntity> entities ) {
+            Logging.BannedEntities bannedEnts = new Logging.BannedEntities();
+            await bannedEnts.LogNewBannedEntities( entities );
+            string reason = $"Banned {string.Join( ",", entities.Select( e => e.EntityString + ":" + e.BanReason ) )} by {string.Join( ",", entities.Select( e => e.BannedBy ).Distinct() )}";
+            if ( reason.Length > 255 ) reason = $"Banned {string.Join( ",", entities.Select( e => e.EntityString ) )} for {string.Join( ",", entities.Select( e => e.BanReason ).Distinct() )} by {string.Join( ",", entities.Select( e => e.BannedBy ).Distinct() )}";
+            if ( reason.Length > 255 ) reason = "Banned lots of things and the summary is too long for the description.. RIP";
+
+            bool done = false;
+            int count = 1;
+            while ( !done && count < 5 ) {
+                try {
+                    done = await SaveAutoModConfig( reason );
+                }
+                catch ( WebException ex ) {
+                    if ( ( ex.Response as HttpWebResponse ).StatusCode == HttpStatusCode.Forbidden ) {
+                        throw;
+                    }
+                    else count++;
+                }
+
+            }
 
             return true;
         }
 
-        private bool SaveBanList(IEnumerable<string> entities ) {
-            string newWikiContent = wikiContent.Substring( 0, startBotSection - 1 );
-        }
 
-        private string GetAutomodConfig() {
+        private async Task<bool> SaveAutoModConfig( string reason ) {
 
             RedditSharp.WikiPage automodWiki;
             try {
@@ -49,14 +65,33 @@ namespace DirtBag.BotFunctions {
                     throw;
                 }
             }
-
+            string updatedWiki = wikiContent;
             string botConfigSection = "";
-            if ( !wikiContent.Contains( WIKI_SECTION_START_IDENTIFIER ) ) return "";
-            if ( !wikiContent.Contains( WIKI_SECTION_END_IDENTIFIER ) ) return "";
+            bool noStart = false;
+            bool noEnd = false;
+            if ( !wikiContent.Contains( WIKI_SECTION_START_IDENTIFIER ) ) noStart = true;
+            if ( !wikiContent.Contains( WIKI_SECTION_END_IDENTIFIER ) ) noEnd = true;
+
+            if ( noStart && noEnd ) { wikiContent += $@"
+{WIKI_SECTION_START_IDENTIFIER + GetDefaultBotConfigSection() + WIKI_SECTION_END_IDENTIFIER}
+"; }
+            else if ( noStart || noEnd ) throw new Exception( "Wiki contains a start or an end section, but not both" );
+
             startBotSection = wikiContent.IndexOf( WIKI_SECTION_START_IDENTIFIER ) + WIKI_SECTION_START_IDENTIFIER.Length;
             botSectionLength = wikiContent.IndexOf( WIKI_SECTION_END_IDENTIFIER, startBotSection ) - startBotSection;
+            if ( botSectionLength < 0 ) { throw new Exception( "End section identifier is before the start identifier" ); }
             botConfigSection = wikiContent.Substring( startBotSection, botSectionLength );
-            return botConfigSection;
+
+            Logging.BannedEntities bannedEnts = new Logging.BannedEntities();
+            var ents = await bannedEnts.GetBannedEntities( SubReddit.Name );
+            string entsString = string.Join( ", ", ents.Select( e => "\"" + e.EntityString + "\"" ) );
+            updatedWiki = updatedWiki.Remove( startBotSection, botSectionLength );
+            updatedWiki = updatedWiki.Insert( startBotSection, String.Format( GetDefaultBotConfigSection(), entsString ) );
+
+            SubReddit.Wiki.EditPage( AUTOMOD_WIKI_PAGE, updatedWiki, reason: reason );
+            return true;
+
+
         }
 
         private string GetDefaultBotConfigSection() {
