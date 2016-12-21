27a14aa7eea849a88daeb6111ef9b03f305c0c61
Meepster <mattiprice520@gmail.com>
2016-12-20 22:36:54 -0600

    Add request models

--- DirtBag/DirtBagTests/BotFunctions/AutoModWranglerTests.cs
+++ DirtBag/DirtBagTests/BotFunctions/AutoModWranglerTests.cs
@@ -13,23 +13,7 @@ namespace DirtBag.BotFunctions.Tests {
         [TestMethod()]
         public async Task AddToBanListTest() {
 
-            var Agent = new RedditWebAgent();
-            RedditWebAgent.EnableRateLimit = true;
-            RedditWebAgent.RateLimit = RedditWebAgent.RateLimitMode.Burst;
-            RedditWebAgent.RootDomain = "oauth.reddit.com";
-            RedditWebAgent.UserAgent = "DirtBagUnitTests (By meepster23)";
-            RedditWebAgent.Protocol = "https";
-            var Auth = new RedditAuth( Agent );
-
-            Auth.Login();
-            Agent.AccessToken = Auth.AccessToken;
-
-            AutoModWrangler w = new AutoModWrangler( new RedditSharp.Reddit( Agent, true ).GetSubreddit( "GooAway" ) );
-            await w.AddToBanList( new List<Models.BannedEntity>() { new Models.BannedEntity() { BanDate = DateTime.UtcNow, BannedBy = "DirtBagTests", BanReason = "DirtBagTests", EntityString = "DirtBagTests", Type= Models.BannedEntity.EntityType.User, SubName = "GooAway", ThingID = "66666" } } );
-
-            var list = await w.GetBannedList();
-            int id = list.Where( l => l.ThingID == "66666" && l.BannedBy == "DirtBagTests" ).Select(l=>l.ID).First();
-            await w.RemoveFromBanList( id, "DirtBagTests" );
+         
         }
     }
 }
